fe2cea57d608ff0c7aedf9307d4dadf4eb900fe4
mrquatsch <{ID}+{username}@users.noreply.github.com>
2020-01-04 14:16:09 -0600

    Create UserRepository

---
+++ src/main/java/com/mikesterry/springbootbuildingblocks/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mikesterry.springbootbuildingblocks.repositories;
+
+import com.mikesterry.springbootbuildingblocks.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
