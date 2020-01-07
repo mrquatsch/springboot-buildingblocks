78cbff382e29df1a012601294fc814b8f6356ec5
mrquatsch <{ID}+{username}@users.noreply.github.com>
2020-01-06 22:57:55 -0600

    create data.sql to load list of users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into User values(107, 'kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin', 'ssn101', 'kreddy');
+insert into User values(108, 'gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
+insert into User values(109, 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark');
