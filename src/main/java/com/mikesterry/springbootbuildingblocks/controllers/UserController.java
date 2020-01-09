09867db3e02a99ca27391d75e9d56070215b0c34
mrquatsch <{ID}+{username}@users.noreply.github.com>
2020-01-08 23:51:16 -0600

    Add exceptions and provide 201 status on successful user creation

--- src/main/java/com/mikesterry/springbootbuildingblocks/controllers/UserController.java
+++ src/main/java/com/mikesterry/springbootbuildingblocks/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.mikesterry.springbootbuildingblocks.controllers;
 
 import com.mikesterry.springbootbuildingblocks.entities.User;
+import com.mikesterry.springbootbuildingblocks.exceptions.UserExistsException;
+import com.mikesterry.springbootbuildingblocks.exceptions.UserNotFoundException;
 import com.mikesterry.springbootbuildingblocks.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,19 +27,33 @@ public class UserController {
     }
 
     @PostMapping("/createUser")
-    public User createUser(@RequestBody User user) {
-        userService.createUser(user);
-        return user;
+    public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder builder) {
+        try {
+            userService.createUser(user);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(builder.path("/users/{id}").buildAndExpand(user.getId()).toUri());
+            return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        } catch (UserExistsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @GetMapping("/users/{id}")
     public Optional<User> getUserById(@PathVariable("id") Long id) {
-        return userService.getUserById(id);
+        try {
+            return userService.getUserById( id );
+        } catch (UserNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 
     @PutMapping("/users/{id}")
     public User updateUserById(@PathVariable("id") Long id , @RequestBody User user) {
-        return userService.updateUserById(id, user);
+        try {
+            return userService.updateUserById( id, user );
+        } catch (UserNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @DeleteMapping("/users/{id}")
---
+++ src/main/java/com/mikesterry/springbootbuildingblocks/exceptions/UserExistsException.java
@@ -0,0 +1,9 @@
+package com.mikesterry.springbootbuildingblocks.exceptions;
+
+public class UserExistsException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public UserExistsException(String message) {
+        super( message );
+    }
+}
