463468560bb84d7eede49b28c0a8570a7fbd8983
mrquatsch <{ID}+{username}@users.noreply.github.com>
2020-01-26 00:16:51 -0600

    03-Implement HttpRequestMethodNotSupportedException in GEH

--- src/main/java/com/mikesterry/springbootbuildingblocks/exceptions/CustomGlobalExceptionHandler.java
+++ src/main/java/com/mikesterry/springbootbuildingblocks/exceptions/CustomGlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.mikesterry.springbootbuildingblocks.exceptions;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -20,5 +21,14 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
                 ex.getMessage());
         return new ResponseEntity<>(customErrorDetails, HttpStatus.BAD_REQUEST);
 //        return super.handleMethodArgumentNotValid( ex, headers, status, request );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//        return super.handleHttpRequestMethodNotSupported( ex, headers, status, request );
+        CustomErrorDetails customErrorDetails = new CustomErrorDetails(new Date(),
+                "From MethodArgumentNotValid Exception in GEH - Method Not Allowed",
+                ex.getMessage());
+        return new ResponseEntity<>(customErrorDetails, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
