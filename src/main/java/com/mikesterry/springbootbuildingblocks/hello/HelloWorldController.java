eb7f44e25529d3929bf820546dca7c0de3804e95
mrquatsch <{ID}+{username}@users.noreply.github.com>
2020-01-28 00:26:32 -0600

    02-Implement RESTful Service with Internationalization

--- src/main/java/com/mikesterry/springbootbuildingblocks/hello/HelloWorldController.java
+++ src/main/java/com/mikesterry/springbootbuildingblocks/hello/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.mikesterry.springbootbuildingblocks.hello;
 
 import com.mikesterry.springbootbuildingblocks.util.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 
     @GetMapping("/helloworld")
     public String helloWorld() {
@@ -15,5 +24,15 @@ public class HelloWorldController {
     @GetMapping("/helloworld-bean")
     public UserDetails helloWorldBean() {
         return new UserDetails("Mike", "Sterry", "Burnsville");
+    }
+
+    @GetMapping("/hello-int")
+    public String getMessagesInI18nFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+    }
+
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18nFormat2() {
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
     }
 }
